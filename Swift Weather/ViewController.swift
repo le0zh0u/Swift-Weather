64eda95427676c0d479cad2c0adeb4dc496e4efe
leozhou <492753662@qq.com>
2016-02-17 00:32:29 +0800

    update to set default location for simulation

--- Swift Weather/ViewController.swift
+++ Swift Weather/ViewController.swift
@@ -7,19 +7,55 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CLLocationManagerDelegate {
 
+    let locationManager:CLLocationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+//        if(ios8()){
+//            locationManager.requestAlwaysAuthorization()
+//        }
+        ////发送授权申请
+        locationManager.requestAlwaysAuthorization()
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            //允许使用定位服务的话，开启定位服务更新
+            locationManager.startUpdatingLocation()
+            print("定位开始")
+        }
     }
+    
+    func ios8() -> Bool{
+        return UIDevice.currentDevice().systemVersion == "8.0"
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location:CLLocation=locations.last!
+        if location.horizontalAccuracy > 0{
+            print(location.coordinate.latitude)
+            print(location.coordinate.longitude)
+            
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error)
+    }
 
 }
 
